61bd1a59bcafa5f48e625c40748c6ee4f1e4965b
Jeremy <jeremychristopherryan@gmail.com>
2019-10-21 21:32:29 -0400

    Add function for plotting Figure 1 in report

--- code/graph.py
+++ code/graph.py
@@ -87,6 +87,49 @@ def draw_pmf(update_function, steps):
 
         plt.clf()
         hexmap.step()
+        
+def draw_manukyan_pmf(update_function, steps):
+    """ Draws a PMF graph with the results of our cellular automaton,
+        overlayed with the PMF of the Manukyan model and a random distribution.
+    """
+
+    # Initialize hex map
+    hexmap = HexMap(width=100, height=100)
+    hexmap.set_update_function(update_function)
+
+    # Plot initial random distribution
+    df1 = pd.DataFrame({'x': range(7), 'y': update_pmf_values(hexmap, 1)})
+    df2 = pd.DataFrame({'x': range(7), 'y': update_pmf_values(hexmap, 0)})
+    plt.plot('x', 'y', data=df1, linestyle=':', marker='o', color='green', label='Green - RD')
+    plt.plot('x', 'y', data=df2, linestyle=':', marker='o', color='black', label='Black - RD')
+
+    # Run simulation for 1000 steps
+    n_steps = 1000
+    for i in range(n_steps):
+        if i % 100 == 0:
+            print("%s%%" % (i/n_steps*100))
+        hexmap.step()
+
+    # Plot distribution after simulating for 1000 time steps
+    df1 = pd.DataFrame({'x': range(7), 'y': update_pmf_values(hexmap, 1)})
+    df2 = pd.DataFrame({'x': range(7), 'y': update_pmf_values(hexmap, 0)})
+    plt.plot('x', 'y', data=df1, linestyle='-', marker='o', color='green', label='Green - CA')
+    plt.plot('x', 'y', data=df2, linestyle='-', marker='o', color='black', label='Black - CA')
+
+    # Plot hard-coded values from Manukyan paper
+    df1 = pd.DataFrame({'x': range(7), 'y': (0, 2/504, 17/504, 112/504, 241/504, 119/504, 16/504)})
+    df2 = pd.DataFrame({'x': range(7), 'y': (0, 23/504, 118/504, 209/504, 126/504, 26/504, 3/504)})
+    plt.plot('x', 'y', data=df1, linestyle='-.', marker='o', color='green', label='Green - M')
+    plt.plot('x', 'y', data=df2, linestyle='-.', marker='o', color='black', label='Black - M')
+
+    # Format and show plot
+    plt.xlabel('Number of Neighbors')
+    plt.ylabel('Frequency')
+    plt.title("Distribution of Differently-Colored Neighbors by Color")
+    plt.legend(loc=(1.04, 0))
+    plt.subplots_adjust(right=0.7)
+    plt.ylim([0, 0.5])
+    plt.show()
 
 
 draw_pmf(helpers.manukyan, 500)
